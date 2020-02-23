6ea05d99f2bece178fb01cf122bcba761cb147e9
serdar1234 <serdarabad@gmail.com>
2020-02-24 01:01:40 +0300

    Начали вести историю

---
+++ 1.js
@@ -0,0 +1,73 @@
+// alert("Das ist Javascript!");
+
+// let test = prompt("сколько тебе лет?", 18);
+// alert("Тебе "+test+" лет!");
+
+// let answer = confirm("Are you a human?");
+// alert(answer);
+
+// let q1 = prompt("WHat is your name?");
+// alert(q1);
+
+// if (null==undefined) 
+// {
+	// alert("Yoshkin kot!");} else {
+	// alert("Net ya ne Byron!");}
+
+// let q2 = prompt("what is the official name of Javascript?","");
+// if (q2 == "ECMAScript") {alert("yes!")} else {alert("No!")};
+
+// let age = prompt("age?","");
+// if (!(age >= 14 && age <=90)) {alert("correct age")} else {alert("incorrect age")};
+// if (age < 14 || age > 90) {alert("correct")} else {alert("wrong")};
+
+// let adminPassword = "";
+// let login = prompt("Who's there?","");
+// if (login == null || login == "") {
+	// alert("Cancelled");
+// } else if (login == "admin" || login == "Admin" || login == "ADMIN") {
+	// adminPassword = prompt("Password","");
+	// if (adminPassword == "qwerty") {
+		// alert(`Hello, ${login}`)} else if (adminPassword == null) {
+		// alert("Cancelled")} else {
+		// alert("Wrong password!")} 
+// } else {
+	// alert("Bugger off, I don't know you!")
+// }
+
+// for (i=2;i<=10;i+=2) {
+// alert(i);
+// }
+
+// for (let i=2;i<=10;i++) {
+
+	// if (i%2==0) {
+	// alert(i);
+	// }
+// }
+
+// let i=0;
+// while (i<3) {
+  // alert( `number ${i}!` );
+  // i++;
+// }
+
+// let i;
+// do {i = prompt("Введи число больше 100","");}
+// while(i<=100 && i);
+
+let n=prompt("number bigger than 1",2);
+for (i=1;i<n;i++) {
+	let foo=1;
+	while (foo<i) {
+	// if(i%foo==0)continue;
+	alert(i);
+	foo++;
+	}
+}
+
+
+
+
+
+
